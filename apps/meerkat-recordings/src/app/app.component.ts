4c8607a1dbf2929efc46a58d40b0862aa180432c
Peter Ericsson <peter.ericsson@addlevel.se>
2019-11-27 10:20:30 +0100

    initial commit

---
+++ apps/meerkat-recordings/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meerkat-recordings-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'meerkat-recordings';
+}
