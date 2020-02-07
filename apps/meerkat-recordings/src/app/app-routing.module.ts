869a2475e1001111866dcc80010fdb09409a2f7e
Peter Ericsson <peter.ericsson@addlevel.se>
2020-02-07 13:48:52 +0100

    npm update

--- apps/meerkat-recordings/src/app/app-routing.module.ts
+++ apps/meerkat-recordings/src/app/app-routing.module.ts
@@ -17,16 +17,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSliderModule } from '@angular/material/slider';
@@ -64,16 +56,8 @@ const routes: Routes = [
     MatDialogModule,
     MatDividerModule,
     MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
     MatListModule,
-    MatMenuModule,
     MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
     MatRippleModule,
     MatSelectModule,
     MatSidenavModule,
@@ -105,16 +89,8 @@ const routes: Routes = [
     MatDialogModule,
     MatDividerModule,
     MatExpansionModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
     MatListModule,
-    MatMenuModule,
     MatNativeDateModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
     MatRippleModule,
     MatSelectModule,
     MatSidenavModule,
