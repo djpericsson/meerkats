5435eea0ca35d8703ebaadaaf33e50450942d1bc
Peter Ericsson <peter.ericsson@addlevel.se>
2020-02-14 08:21:06 +0100

    minimize json files

--- apps/meerkat-recordings/src/app/model/irelease.ts
+++ apps/meerkat-recordings/src/app/model/irelease.ts
@@ -4,7 +4,6 @@ export interface IRelease {
   name: string
   artist: string
   date: string
-  description: string
   url: string
   img: string
   artistImg: string
--- apps/meerkat-recordings/src/app/model/isong.ts
+++ apps/meerkat-recordings/src/app/model/isong.ts
@@ -1,7 +1,5 @@
 export interface ISong {
-  number: string
   name: string
   artist: string
-  length: string
   songs: string[]
 }
