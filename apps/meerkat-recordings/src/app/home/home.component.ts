55566887acb0b20efe217d413f152defbaec241c
Peter Ericsson <peter.ericsson@addlevel.se>
2020-08-20 11:18:59 +0200

    destroy

--- apps/meerkat-recordings/src/app/home/home.component.ts
+++ apps/meerkat-recordings/src/app/home/home.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GoogleAnalyticsService } from '../service/google-analytics.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'meerkat-recordings-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  unsubscribe$ = new Subject<void>()
   constructor(
     public googleAnalyticsService: GoogleAnalyticsService
   ) {}
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.unsubscribe$.next()
+    this.unsubscribe$.complete()
+  }
 
   onClick(url: string) {
     this.googleAnalyticsService.eventEmitter(
